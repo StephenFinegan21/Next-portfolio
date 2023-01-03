b66052e3ea4fc018143708f63253df42e662a473
StephenFinegan21 <x20191952@student.ncirl.com>
2023-01-03 23:50:13 +0000

    popover basic state added

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,12 +1,26 @@
-import Link from 'next/link'
+"use client";
+
+import Link from "next/link";
+import { Popover } from "@headlessui/react";
+import {IoMenuSharp} from 'react-icons/io5'
 
 export default function Navigation() {
-    return (
-        <>
-            <div className="flex flex-row text-white py-4 w-full  justify-end		">
-                <Link className='mr-4' href="/">About</Link>
-                <Link href="/projects">Projects</Link>
-            </div>
-        </>
-    )
+  return (
+    <>
+  
+      <Popover className="relative flex  text-white py-4 w-full justify-end">
+        <Popover.Button className='text-white text-2xl'><IoMenuSharp /></Popover.Button>
+
+        <Popover.Panel className=" absolute  top-12 bg-zinc-900 flex justify-end h-14 z-10">
+          <div className=" text-white text-right flex flex-col justify-center align-middle">
+            <Link href="/">
+              About
+            </Link>
+            <Link href="/projects">Projects</Link>
+          </div>
+        </Popover.Panel>
+      </Popover>
+  
+    </>
+  );
 }
