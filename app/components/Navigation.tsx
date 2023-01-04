c961fae1860512055224a192567b3cbab1ff79b5
StephenFinegan21 <x20191952@student.ncirl.com>
2023-01-04 22:46:36 +0000

    changing menu that appears based on screen size

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -12,8 +12,12 @@ export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
 
   const variants = {
-    open: { opacity: 1, x: 0, transition: { delay: 0.2 } },
-    closed: { opacity: 0.6, x: 500, transition: { delay: 0.2 } },
+    open: { opacity: 1, x: 0, transition: { duration: 0.2 } },
+    closed: {
+      opacity: 0.6,
+      x: 500,
+      transition: { type: "spring", duration: 1 },
+    },
   };
 
   const links = [
@@ -27,12 +31,25 @@ export default function Navigation() {
     },
   ];
 
-  console.log(isOpen);
-
   const path = usePathname();
   return (
     <>
-      <div className="relative flex  text-white py-4 w-full justify-end">
+      <div className="hidden md:flex gap-4 w-full justify-end items-center">
+      {links.map((link) => (
+              <>
+                <Link
+                  href={link.href}
+                  className={`"w-full py-1  text-right text-white border-y-1" ${
+                    path === link.href ? "text-emerald-400" : ""
+                  }`}
+                >
+                  {link.name}
+                </Link>
+                <div className="bg-zinc-700 h-px"></div>
+              </>
+            ))}
+      </div>
+      <div className="md:hidden relative flex  text-white py-4 w-full justify-end">
         <button
           className="text-white text-2xl"
           onClick={() => setIsOpen((isOpen) => !isOpen)}
@@ -42,7 +59,7 @@ export default function Navigation() {
       </div>
 
       <motion.div
-        className="fixed top-8 right-4 w-48 p-8 rounded-md z-10 ring-1 bg-zinc-900 ring-zinc-700 opacity-100"
+        className="fixed top-8 right-4 w-48 p-8 rounded-md z-10 ring-1 bg-zinc-900 ring-zinc-600"
         animate={isOpen ? "open" : "closed"}
         variants={variants}
       >
