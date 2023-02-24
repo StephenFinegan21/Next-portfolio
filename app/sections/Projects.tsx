0ba263db480fc861b91741a2c933bf958a384a8e
StephenFinegan21 <x20191952@student.ncirl.com>
2023-02-24 20:13:05 +0000

    updated site styling and layout

--- app/sections/Projects.tsx
+++ app/sections/Projects.tsx
@@ -5,7 +5,7 @@ export default function Projects() {
  
     return (
         <>
-        <div className="md:h-[80vh] md:flex flex-col items-center justify-center my-24 md:my-0">
+        <div className="md:h-[80vh]  md:flex flex-col items-center justify-center my-24 ">
             <h1 className="text-3xl font-bold py-4 tracking-tight text-white sm:text-5xl ">Projects</h1>
             <div>
                 {projects.map(project =>
--- app/sections/Skills.tsx
+++ app/sections/Skills.tsx
@@ -10,7 +10,7 @@ export default function Skills() {
       
   return (
     <>
-      <div className="md:h-[80vh] md:flex flex-col items-center justify-center  w-full md:w-2/5 md:mx-auto my-24 md:my-0">
+      <div className="md:h-[80vh] md:flex flex-col items-center justify-center  w-full md:w-2/5 md:mx-auto my-24 ">
         {skills.map((skill) => (
           <motion.div
             initial={{ opacity: 0, x: -40 }}
@@ -20,7 +20,7 @@ export default function Skills() {
             className="flex w-full  gap-4 items-center py-4  md:pt-12 md:pb-2 text-zinc-100 border-b   border-zinc-300"
           >
             {/*<img className="w-8 col-span-1 filter brightness-[90%]" src={skill.image} />*/}
-            <p className="text-white col-span-4 text-lg md:text-4xl md:font-semibold font-archivo">{skill.name}</p>
+            <p className="text-white col-span-4 text-lg md:text-2xl md:font-semibold font-archivo">{skill.name}</p>
           </motion.div>
         ))}
       </div>
