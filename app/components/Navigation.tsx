f6f17c91e017620808bf5c429180d84b1abf3252
StephenFinegan21 <x20191952@student.ncirl.com>
2023-07-03 13:46:35 +0100

    debugging prod

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,7 +5,8 @@ import { useState } from "react";
 
 
 export default function Navigation() {
-  const [activeLink, setActiveLink] = useState<number>(0);
+  const [activeLink, setActiveLink] = useState<number>();
+  console.log(activeLink)
 
   const handleSetActiveLink = (index:number) => {
     setActiveLink(index);
@@ -15,7 +16,7 @@ export default function Navigation() {
     <ul className="flex mx-auto gap-4 lg:gap-8 rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
       <li>
         <div
-          className={`nav-link-wrapper ${activeLink === 0 ? "active" : ""}`}
+          className={`nav-link-wrapper ${activeLink === 0 || !activeLink  ? "active" : ""}`}
           onClick={() => handleSetActiveLink(0)}
         >
           <Link href="/">About</Link>
