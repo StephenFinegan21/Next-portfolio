ca1a3077e662e2808a06b19ee73c96e338c0cf52
StephenFinegan21 <x20191952@student.ncirl.com>
2023-07-03 15:26:54 +0100

    debugging prod

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,11 +1,20 @@
-"use client";
+'use client'
 
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
+
 import { useState } from "react";
 
 
 export default function Navigation() {
   const [activeLink, setActiveLink] = useState<number>();
+  const navLinks = [
+    { name: 'About', href: '/' },
+    { name: 'Notes', href: '/notes' },
+    { name: 'Projects', href: '/projects' },
+  ];
+  const pathname = usePathname()
+  console.log(pathname)
   console.log(activeLink)
 
   const handleSetActiveLink = (index:number) => {
@@ -14,30 +23,19 @@ export default function Navigation() {
 
   return (
     <ul className="flex mx-auto gap-4 lg:gap-8 rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-      <li>
-        <div
-          className={`nav-link-wrapper ${activeLink === 0 || !activeLink  ? "active" : ""}`}
-          onClick={() => handleSetActiveLink(0)}
-        >
-          <Link href="/">About</Link>
-        </div>
-      </li>
-      <li>
-        <div
-          className={`nav-link-wrapper ${activeLink === 1 ? "active" : ""}`}
-          onClick={() => handleSetActiveLink(1)}
-        >
-          <Link href="/notes">Notes</Link>
-        </div>
-      </li>
-      <li>
-        <div
-          className={`nav-link-wrapper ${activeLink === 2 ? "active" : ""}`}
-          onClick={() => handleSetActiveLink(2)}
-        >
-          <Link href="/projects">Projects</Link>
-        </div>
-      </li>
+    {navLinks.map((link) => {
+        const isActive = pathname && pathname === link.href
+ 
+        return (
+          <Link
+            className={isActive ? 'underline' : ' '}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        )
+      })}
     </ul>
   );
 }
